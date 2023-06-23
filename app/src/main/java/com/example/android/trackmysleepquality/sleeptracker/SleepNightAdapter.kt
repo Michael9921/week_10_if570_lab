467dab803004c4e0cb443eb56c028a3bdbbc8526
Michael9921 <michael.dharmawan1@student.umn.ac.id>
2023-06-23 11:53:36 +0700

    Improving code

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -21,6 +21,31 @@ class SleepNightAdapter: RecyclerView.Adapter<SleepNightAdapter.ViewHolder>() {
         val sleepLength: TextView = itemView.findViewById(R.id.sleep_length)
         val quality: TextView = itemView.findViewById(R.id.quality_string)
         val qualityImage: ImageView = itemView.findViewById(R.id.quality_image)
+        fun bind(item: SleepNight) {
+            val res = itemView.context.resources
+            sleepLength.text = convertDurationToFormatted(
+                item.startTimeMilli, item.endTimeMilli, res)
+            quality.text = convertNumericQualityToString(
+                item.sleepQuality, res)
+            qualityImage.setImageResource(when (item.sleepQuality) {
+                0 -> R.drawable.ic_sleep_0
+                1 -> R.drawable.ic_sleep_1
+                2 -> R.drawable.ic_sleep_2
+                3 -> R.drawable.ic_sleep_3
+                4 -> R.drawable.ic_sleep_4
+                5 -> R.drawable.ic_sleep_5
+                else -> R.drawable.ic_sleep_active
+            })
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): ViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val view = layoutInflater
+                    .inflate(R.layout.list_item_sleep_night, parent, false)
+                return ViewHolder(view)
+            }
+        }
     }
 //    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){*
 //        val sleepLength: TextView = itemView.findViewById(R.id.sleep_length)
@@ -31,8 +56,10 @@ class SleepNightAdapter: RecyclerView.Adapter<SleepNightAdapter.ViewHolder>() {
         parent: ViewGroup, viewType: Int): ViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
         val view = layoutInflater.inflate(R.layout.list_item_sleep_night, parent, false)
-        return ViewHolder(view)
+        return from(view)
     }
+
+    private fun from(view: View) = ViewHolder(view)
 
 //    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
 //        val layoutInflater = LayoutInflater.from(parent.context)
@@ -52,11 +79,31 @@ class SleepNightAdapter: RecyclerView.Adapter<SleepNightAdapter.ViewHolder>() {
 //            holder.textView.setTextColor(Color.BLACK) // black
 //        }
 //    }
+//    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+//    val item = bind(position)
+//    val res = holder.itemView.context.resources
+//        holder.sleepLength.text = convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, res)
+//        holder.quality.text= convertNumericQualityToString(item.sleepQuality, res)
+//        holder.qualityImage.setImageResource(when (item.sleepQuality) {
+//            0 -> R.drawable.ic_sleep_0
+//            1 -> R.drawable.ic_sleep_1
+//            2 -> R.drawable.ic_sleep_2
+//            3 -> R.drawable.ic_sleep_3
+//            4 -> R.drawable.ic_sleep_4
+//            5 -> R.drawable.ic_sleep_5
+//            else -> R.drawable.ic_sleep_active
+//        })
+//    }
+
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = data[position]
+        holder.bind(item)
+    }
+
+    private fun bind(holder: ViewHolder, item: SleepNight) {
         val res = holder.itemView.context.resources
         holder.sleepLength.text = convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, res)
-        holder.quality.text= convertNumericQualityToString(item.sleepQuality, res)
+        holder.quality.text = convertNumericQualityToString(item.sleepQuality, res)
         holder.qualityImage.setImageResource(when (item.sleepQuality) {
             0 -> R.drawable.ic_sleep_0
             1 -> R.drawable.ic_sleep_1
